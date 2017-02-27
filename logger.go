6ec2a5c58c4f98b87e3a3891b1dc3f9c4a24a6fc
Tomaz Toplak <tomaz@arabsu.com>
2017-02-27 17:07:27 +0100

    Logger wrapper for the handler functions

---
+++ logger.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+func Logger(inner http.Handler, name string) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        //time of the incoming request so we can calculate the duration until response
+        start := time.Now()
+
+        inner.ServeHTTP(w, r)
+
+        //output to the console
+        log.Printf(
+            "%s\t%s\t%s\t%s",
+            r.Method,
+            r.RequestURI,
+            name,
+            time.Since(start),
+        )
+    })
+}
--- router.go
+++ router.go
@@ -1,6 +1,8 @@
 package main
 
 import ( 
+    "net/http"
+
     /*Nice router from the gorilla web toolkit: http://www.gorillatoolkit.org/pkg/mux*/
     "github.com/gorilla/mux"
 )
@@ -10,11 +12,16 @@ func NewRouter() *mux.Router {
     //use the mux router to find the correct handlers
     router := mux.NewRouter().StrictSlash(true)
     for _, route := range routes {
+        var handler http.Handler
+
+        //wrap the handler funcion with the logger
+        handler = Logger(route.HandlerFunc, route.Name)    
+    
         router.
             Methods(route.Method).
             Path(route.Pattern).
             Name(route.Name).
-            Handler(route.HandlerFunc)
+            Handler(handler)
     }
     return router
 }
