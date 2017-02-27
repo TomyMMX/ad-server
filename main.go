25b74db7c8798871c1e02c923ac32eef2018953b
Tomaz Toplak <tomaz@arabsu.com>
2017-02-27 12:37:43 +0100

    Added a better router

--- main.go
+++ main.go
@@ -5,13 +5,22 @@ import (
     "html"
     "log"
     "net/http"
+    /*Nice router from the gorilla web toolkit: http://www.gorillatoolkit.org/pkg/mux*/
+    "github.com/gorilla/mux"
 )
 
 //basic server
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
+    
+    //use the mux router to run the Index hanlder when the / endpoint is called
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    http.Handle("/", router)
 
+    //run the server on por 8080
     log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
