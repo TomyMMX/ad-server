ee3ac1967f8934e1841c40ed12e2a8a7316eaac6
Tomaz Toplak <tomaz@arabsu.com>
2017-02-28 22:46:02 +0100

    Updated DB schema.

--- DB/dbschema.sql
+++ DB/dbschema.sql
@@ -1,20 +1,21 @@
-CREATE DATABASE `addb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE `addb`;
 
 CREATE TABLE `folder` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parrentid` int(11) NOT NULL DEFAULT '0',
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `lastmodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE `ad` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `folderid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `url` varchar(512) CHARACTER SET ascii NOT NULL,
-  `lastmodified` datetime NOT NULL,
+  `lastmodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `folderId_idx` (`folderid`),
   CONSTRAINT `folderId` FOREIGN KEY (`folderid`) REFERENCES `folder` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
